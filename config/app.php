a6d71d68fee42e15c0d2db6da3e7078eb0eaa92e
cavid24 <Bitbucket9099xy>
2019-03-14 04:14:11 +0400

    Setting show target ships on board

---
+++ config/app.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kcavi
+ * Date: 14.03.2019
+ * Time: 4:11
+ */
+
+return [
+    'show_target_ships' => 0 // show target ships on html battle board
+];
--- routes/index.php
+++ routes/index.php
@@ -9,7 +9,7 @@
     <ul>
         <li>1. Open <b>config/database.php</b> file and replace database credentials with yours</li>
         <li>2. Start install.php file (Example: <b>http://yoursite.com/install.php</b>) or <a href="install.php">Click Here</a></li>
-        <li>3. By default target ships colors are visible. Just remove < ? = ($value) == 1 ? 'bg-danger':'' ? > code from routes/game/index.php file</li>
+        <li>3. By default target ships colors are visible. Just set show_target_ships to 0 in config/app.php file</li>
     </ul>
 
     <h4>What and Where</h4>
